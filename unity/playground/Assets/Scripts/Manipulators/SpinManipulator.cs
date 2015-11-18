f9b629ad85d27a19800d33b372c124bdaa15c071
Jonathan Thorpe <thorpe@lri.fr>
2015-11-18 17:45:36 +0100

    [unity] added spin manipulator

---
+++ unity/playground/Assets/Scripts/Manipulators/SpinManipulator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinManipulator : SowizManipulator {
+
+	public Vector3 axis = new Vector3(1.0f, 0.0f, 0.0f);
+	public float scale = 180.0f;
+
+	private float magnitude = 0.0f;
+	
+	void Awake() {
+		descriptors = new string[] {"magnitude"};	
+	}
+
+	void Update() {
+		target.transform.rotation *= Quaternion.AngleAxis ( (float) (scale * magnitude * Time.deltaTime * 60.0f), axis );
+	}
+
+	public override void ApplyMessage(SowizControlMessage message) {
+		
+		switch (message.descriptor) {
+			
+		case "magnitude":
+			SetMagnitude ((float) message.values[0]);
+			break;
+		default:
+			break;
+			
+		}
+		
+	}
+	
+	private void SetMagnitude(float m) {
+		Debug.Log ("Setting magnitude to " + m.ToString ());
+		magnitude = m;
+	}
+}
