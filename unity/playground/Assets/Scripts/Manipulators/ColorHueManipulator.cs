a7f3a10ead82977ff5c1a09be7d2ce16a0e0e804
Jonathan Thorpe <thorpe@lri.fr>
2015-11-20 16:59:34 +0100

    [unity] starting on color manipulators.

---
+++ unity/playground/Assets/Scripts/Manipulators/ColorHueManipulator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorHueManipulator : SowizManipulator {
+
+	public int zeroHue = 0;
+	public int unitHue = 128;
+	
+	void Awake() {
+		descriptors = new string[] {"magnitude"};	
+	}
+	
+	public override void ApplyMessage(SowizControlMessage message) {
+		
+		switch (message.descriptor) {
+			
+		case "magnitude":
+			SetMagnitude ((float) message.values[0]);
+			break;
+		default:
+			break;
+			
+		}
+		
+	}
+	
+	private void SetMagnitude(float m) {
+		
+		Debug.Log ("Setting magnitude to " + m.ToString ());
+
+		Renderer renderer = target.GetComponent<Renderer> ();
+
+		Color currentColor = renderer.material.GetColor ("_MainTint");
+
+		Debug.Log ("Current color is " + currentColor.ToString ());
+	}
+
+}
