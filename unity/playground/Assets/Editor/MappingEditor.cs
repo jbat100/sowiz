4f6bd0db391426abae48ed7b6f87be011021e70e
Jonathan Thorpe <johnny.frenchy@gmail.com>
2016-03-14 15:19:42 +0100

    [unity] removed Editor folder from gitignore, nearly finished splines tutorial

---
+++ unity/playground/Assets/Editor/MappingEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SowizManipulator))]
+public class MappingEditor : Editor {
+		
+		public override void OnInspectorGUI() {
+				SowizManipulator manipulator = (SowizManipulator)target;
+		}
+	
+}
--- unity/playground/Assets/Scripts/Utils/BezierCurve.cs
+++ unity/playground/Assets/Scripts/Utils/BezierCurve.cs
@@ -22,6 +22,25 @@ public static class Bezier {
 			2f * (1f - t) * (p1 - p0) +
 			2f * t * (p2 - p1);
 	}
+
+	public static Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * oneMinusT * p0 +
+			3f * oneMinusT * oneMinusT * t * p1 +
+			3f * oneMinusT * t * t * p2 +
+			t * t * t * p3;
+	}
+
+	public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			3f * oneMinusT * oneMinusT * (p1 - p0) +
+			6f * oneMinusT * t * (p2 - p1) +
+			3f * t * t * (p3 - p2);
+	}
 }
 
 public class BezierCurve : MonoBehaviour {
@@ -32,17 +51,21 @@ public class BezierCurve : MonoBehaviour {
 		points = new Vector3[] {
 			new Vector3(1f, 0f, 0f),
 			new Vector3(2f, 0f, 0f),
-			new Vector3(3f, 0f, 0f)
+			new Vector3(3f, 0f, 0f),
+			new Vector3(4f, 0f, 0f)
 		};
 	}
 
 	public Vector3 GetPoint (float t) {
-		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], t));
+		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
 	}
 
 	public Vector3 GetVelocity (float t) {
-		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], t)) 
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) 
 			- transform.position;
 	}
 
+	public Vector3 GetDirection (float t) {
+		return GetVelocity(t).normalized;
+	}
 }
