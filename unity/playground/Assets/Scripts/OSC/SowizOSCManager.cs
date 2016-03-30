3561646d5ef5c83dab6a4d70a41bd3563916fbed
Jonathan Thorpe <johnny.frenchy@gmail.com>
2016-03-30 17:57:38 +0200

    [unity] added osc buffer to avoid limit locking on the main thread, not sure how InvokeRepeating works, we may need an extra thread to get the wanted behaviour.

--- unity/playground/Assets/Scripts/OSC/SowizOSCManager.cs
+++ unity/playground/Assets/Scripts/OSC/SowizOSCManager.cs
@@ -3,266 +3,299 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-class ControlMessageQueue {
-
-		private readonly object syncLock = new object();
-		private List<SowizControlMessage> queue;
-
-		public int Count
-		{
-				get
-				{
-						lock(syncLock) 
-						{
-								return queue.Count;
-						}
-				}
-		}
-
-		public ControlMessageQueue()
-		{
-				this.queue = new List<SowizControlMessage>();
-		}
-
-		public SowizControlMessage Peek()
-		{
-				lock(syncLock)
-				{
-						return queue[0];
-				}
-		}	
-
-		public void Enqueue(SowizControlMessage message)
-		{
-				lock(syncLock)
-				{
-						int index = -1;
-						for (int i = 0; i < queue.Count; i++)
-						{
-								SowizControlMessage m = queue[i];
-								if (m.sameTarget(message)) 
-								{
-										index = i;
-										break;
-								}
-						}
-						if (index == -1) 
-						{
-								queue.Add(message);
-						} 
-						else 
-						{
-								queue[index] = message;
-						}
-				}
-		}
-
-		public SowizControlMessage Dequeue()
-		{
-				lock(syncLock)
-				{
-						if (queue.Count > 0) {
-								SowizControlMessage message = queue[0];
-								queue.RemoveAt(0);
-								return message;
-						}
-						return null;
-				}
-		}
-
-		public List<SowizControlMessage> DequeueAll()
-		{
-				lock(syncLock)
-				{
-
-						List<SowizControlMessage> copied = new List<SowizControlMessage>(queue);			
-						queue.Clear();
-						return copied;
-				}
-		}
-
-		public void Clear()
-		{
-				lock(syncLock)
-				{
-						queue.Clear();
-				}
-		}
-
-		public SowizControlMessage[] CopyToArray()
-		{
-				lock(syncLock)
-				{
-						if(queue.Count == 0)
-						{
-								return new SowizControlMessage[0];
-						}
-
-						SowizControlMessage[] values = new SowizControlMessage[queue.Count];
-						queue.CopyTo(values, 0);	
-						return values;
-				}
-		}
-
-		public static ControlMessageQueue InitFromArray(IEnumerable<SowizControlMessage> initValues)
-		{
-				var queue = new ControlMessageQueue();
-
-				if(initValues == null)	
-				{
-						return queue;
-				}
-
-				foreach(SowizControlMessage val in initValues)
-				{
-						queue.Enqueue(val);
-				}
-
-				return queue;
-		}
-}
-
-
-
 public class SowizControlMessage
 {
-		public string descriptor;
-		public string group;
+	public string descriptor;
+	public string group;
 
-		public ArrayList values;
+	public ArrayList values;
 
-		public SowizControlMessage(string _group, string _descriptor, ArrayList _values) {
-				descriptor = _descriptor;
-				group = _group;
-				values = _values;
+	public SowizControlMessage(string _group, string _descriptor, ArrayList _values) {
+		descriptor = _descriptor;
+		group = _group;
+		values = _values;
+	}
+
+	public override string ToString()
+	{
+		//Debug.Log("group is " + group);
+		return "SowizRoutingParameters (descriptor: " + descriptor + ", group: " + group + ")";
+	}
+
+	public bool sameTarget(SowizControlMessage message) {
+		if (!message.descriptor.Equals (descriptor) || !message.group.Equals (group)) {
+			return false;
 		}
+		return true;
+	}
 
-		public override string ToString()
+	public static SowizControlMessage FromOscMessage(OscMessage message) {
+
+		string[] elements = message.Address.Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+		//Debug.Log("RoutingParametersForMessage elements are " + elements.ToString());
+
+		if (elements.Length < 2) 
 		{
-				//Debug.Log("group is " + group);
-				return "SowizRoutingParameters (descriptor: " + descriptor + ", group: " + group + ")";
+			Debug.Log("DefaultMessageCallback unexpected elements length");
+			return null;
 		}
 
-		public bool sameTarget(SowizControlMessage message) {
-				if (!message.descriptor.Equals (descriptor) || !message.group.Equals (group)) {
-						return false;
-				}
-				return true;
+		return new SowizControlMessage (elements [0], elements [1], message.Values);
+	}
+};
+
+public class SowizControlMessageQueue {
+
+	private readonly object syncLock = new object();
+	private List<SowizControlMessage> queue;
+
+	public int Count
+	{
+		get
+		{
+			lock(syncLock) 
+			{
+				return queue.Count;
+			}
 		}
+	}
+
+	public SowizControlMessageQueue()
+	{
+		this.queue = new List<SowizControlMessage>();
+	}
+
+	public SowizControlMessage Peek()
+	{
+		lock(syncLock)
+		{
+			return queue[0];
+		}
+	}	
+
+	private void UnprotectedEnqueue (SowizControlMessage message) {
+
+		int index = -1;
+		for (int i = 0; i < queue.Count; i++)
+		{
+			SowizControlMessage m = queue[i];
+			if (m.sameTarget(message)) 
+			{
+				index = i;
+				break;
+			}
+		}
+		if (index == -1) 
+		{
+			queue.Add(message);
+		} 
+		else 
+		{
+			queue[index] = message;
+		}
+		
+	}
+
+	public void Enqueue(SowizControlMessage message)
+	{
+		lock(syncLock)
+		{
+			this.UnprotectedEnqueue(message);
+		}
+	}
+
+	public void TransferFrom(SowizControlMessageQueue otherQueue)
+	{
+		lock(syncLock)
+		{
+			List<SowizControlMessage> transfered = otherQueue.DequeueAll();
+
+			if (transfered.Count > 0) {
+				Debug.Log("Transferring " + transfered.Count + " control messages");
+			}
+
+			foreach(SowizControlMessage message in transfered)
+			{
+				this.UnprotectedEnqueue(message);
+			}
+		}
+	}
+
+	public SowizControlMessage Dequeue()
+	{
+		lock(syncLock)
+		{
+			if (queue.Count > 0) {
+				SowizControlMessage message = queue[0];
+				queue.RemoveAt(0);
+				return message;
+			}
+			return null;
+		}
+	}
+
+	public List<SowizControlMessage> DequeueAll()
+	{
+		lock(syncLock)
+		{
+			List<SowizControlMessage> copied = new List<SowizControlMessage>(queue);			
+			queue.Clear();
+			return copied;
+		}
+	}
+
+	public void Clear()
+	{
+		lock(syncLock)
+		{
+			queue.Clear();
+		}
+	}
+
+	public SowizControlMessage[] CopyToArray()
+	{
+		lock(syncLock)
+		{
+			if(queue.Count == 0)
+			{
+				return new SowizControlMessage[0];
+			}
+
+			SowizControlMessage[] values = new SowizControlMessage[queue.Count];
+			queue.CopyTo(values, 0);	
+			return values;
+		}
+	}
+
+	public static SowizControlMessageQueue InitFromArray(IEnumerable<SowizControlMessage> initValues)
+	{
+		var queue = new SowizControlMessageQueue();
+
+		if(initValues == null)	
+		{
+			return queue;
+		}
+
+		foreach(SowizControlMessage val in initValues)
+		{
+			queue.Enqueue(val);
+		}
+
+		return queue;
+	}
+};
+	
+
+public class SowizControlMessageBuffer {
+
+	public Osc handler;
+
+	private SowizControlMessageQueue bufferQueue;
+
+	public SowizControlMessageBuffer(Osc _handler) {
+		
+		bufferQueue = new SowizControlMessageQueue ();
+		handler = _handler;
+		handler.SetAllMessageHandler(MessageCallback);
+
+	}
+
+	public void TransferTo(SowizControlMessageQueue targetQueue) {
+		
+		targetQueue.TransferFrom(bufferQueue);
+
+	}
+
+	void MessageCallback (OscMessage oscMessage) {
+		
+		//Debug.Log("DefaultMessageCallback received message " + message.Address + ' ' + message.Values[0]);
+		SowizControlMessage sowizMessage = SowizControlMessage.FromOscMessage (oscMessage);
+		if (sowizMessage != null) 
+		{
+			Debug.Log("Received message : " + sowizMessage.ToString() );
+			bufferQueue.Enqueue(sowizMessage);
+		}
+
+	}
 
 };
 
 public class SowizOSCManager : MonoBehaviour {
 
-		public string remoteIP = "127.0.0.1";
-		public int remotePort = 9123;
-		public int localPort = 3333;
-		private Osc handler;
+	public string remoteIP = "127.0.0.1";
+	public int remotePort = 9123;
+	public int localPort = 3333;
 
-		private GameObject[] sowizObjects;
-		private ControlMessageQueue messageQueue = new ControlMessageQueue ();
-		private int updateCount = 0;
+	private Osc handler;
+	private SowizControlMessageBuffer messageBuffer;
+	private SowizControlMessageQueue messageQueue;
 
-		// Use this for initialization
-		void Start () {
+	private SowizManipulator[] manipulators;
+	private int updateCount = 0;
+	private float period = 0.017f;
 
-				//Initializes on start up to listen for messages
-				//make sure this game object has both UDPPackIO and OSC script attached
-				UDPPacketIO udp = (UDPPacketIO)GetComponent("UDPPacketIO");
-				udp.init(remoteIP, remotePort, localPort);
-				handler = (Osc)GetComponent("Osc");
-				handler.init(udp);
+	// Use this for initialization
+	void Start () {
 
-				//handler.SetAddressHandler("/sowiz/scene/descriptor", SceneDescriptorMessageCallback);
-				handler.SetAllMessageHandler(DefaultMessageCallback);	
+		//Initializes on start up to listen for messages
+		//make sure this game object has both UDPPackIO and OSC script attached
+		UDPPacketIO udp = (UDPPacketIO)GetComponent("UDPPacketIO");
+		udp.init(remoteIP, remotePort, localPort);
+		handler = (Osc)GetComponent("Osc");
+		handler.init(udp);
 
-				sowizObjects = GameObject.FindGameObjectsWithTag("Sowiz");
+		messageQueue = new SowizControlMessageQueue ();
+		messageBuffer = new SowizControlMessageBuffer(handler);
+
+		// get all game objects with maniulator components
+		//manipulators = gameObject.GetComponents<SowizManipulator>();
+
+		//HingeJoint[] hinges = FindObjectsOfType(typeof(HingeJoint)) as HingeJoint[];
+
+		manipulators = FindObjectsOfType(typeof(SowizManipulator)) as SowizManipulator[];
+
+		//manipulators = (SowizManipulator[])FindObjectOfType<SowizManipulator>();
+
+		Debug.Log("Found " + manipulators.Length + " manipulators");
+
+		InvokeRepeating("ReadBuffer", period, period);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		updateCount++;
+
+		List<SowizControlMessage> dequeued = messageQueue.DequeueAll();
+
+		foreach (SowizControlMessage message in dequeued) {
+			ApplyMessage(message);
 		}
 
-		// Update is called once per frame
-		void Update () {
+	}
 
-				updateCount++;
+	void ReadBuffer() {
+		
+		messageBuffer.TransferTo(messageQueue);
 
-				// Debug.Log ("Update " + updateCount.ToString ());
+	}
 
-				/*
+	void OnApplicationQuit() {
+		
+		UDPPacketIO udp = (UDPPacketIO)GetComponent("UDPPacketIO");
+		udp.Close ();
 
-				while (messageQueue.Count > 0) 
-				{
-						try 
-						{
-								SowizControlMessage message = messageQueue.Dequeue();
-								//Debug.Log ("Dequeued control message " + message.ToString() );
-								ApplyMessage(message);
-						} 
-						catch(InvalidOperationException) 
-						{
-								//Debug.Log ("Exception while dequeing message");
-						}
-				}
+	}
 
-*/
-
-				List<SowizControlMessage> dequeued = messageQueue.DequeueAll();
-
-				foreach (SowizControlMessage message in dequeued) {
-						ApplyMessage(message);
-				}
-
+	void ApplyMessage(SowizControlMessage message) {
+		
+		foreach (SowizManipulator manipulator in manipulators) {
+			if ( System.Array.IndexOf(manipulator.groups, message.group) != -1) {
+				manipulator.ApplyMessage(message);
+			}
 		}
 
-		void OnApplicationQuit()
-		{
-				UDPPacketIO udp = (UDPPacketIO)GetComponent("UDPPacketIO");
-				udp.Close ();
-		}
-
-		void DefaultMessageCallback (OscMessage oscMessage) {
-
-				//Debug.Log("DefaultMessageCallback received message " + message.Address + ' ' + message.Values[0]);
-				SowizControlMessage sowizMessage = SowizMessageFromOscMessage (oscMessage);
-				if (sowizMessage != null) 
-				{
-						Debug.Log("Received message with routing parameters : " + sowizMessage.ToString() );
-						// work out how to call Apply from the main thread (doesn't like calling unity engine stuff on the OSC server's thread)
-						messageQueue.Enqueue(sowizMessage);
-				}
-
-		}
-
-		void ApplyMessage(SowizControlMessage message) {
-
-				foreach (GameObject gameObject in sowizObjects) 
-				{
-						SowizManipulator[] manipulators = gameObject.GetComponents<SowizManipulator>();
-						//Debug.Log("Applying message to " + manipulators.Length.ToString() + " manipulators" );
-						foreach (SowizManipulator manipulator in manipulators) 
-						{
-								if ( System.Array.IndexOf(manipulator.groups, message.group) != -1) 
-								{
-										manipulator.ApplyMessage(message);
-								}
-						}
-				}
-
-		}
-
-		SowizControlMessage SowizMessageFromOscMessage(OscMessage message) {
-
-				string[] elements = message.Address.Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-				//Debug.Log("RoutingParametersForMessage elements are " + elements.ToString());
-
-				if (elements.Length < 2) 
-				{
-						Debug.Log("DefaultMessageCallback unexpected elements length");
-						return null;
-				}
-
-				return new SowizControlMessage (elements [0], elements [1], message.Values);
-
-		}
-
-
+	}
 };
