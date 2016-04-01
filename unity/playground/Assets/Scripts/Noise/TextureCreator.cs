c2691d30993fe894d7589c4988701641dfe2969e
Jonathan Thorpe <thorpe@lri.fr>
2016-04-01 19:54:41 +0200

    [unity] completed first noise tutorial and playing around with generated turbulence shaders

--- unity/playground/Assets/Scripts/Noise/TextureCreator.cs
+++ unity/playground/Assets/Scripts/Noise/TextureCreator.cs
@@ -11,6 +11,17 @@ public class TextureCreator : MonoBehaviour {
 	[Range(1, 3)]
 	public int dimensions = 3;
 
+	[Range(1, 8)]
+	public int octaves = 1;
+
+	[Range(1f, 4f)]
+	public float lacunarity = 2f;
+
+	[Range(0f, 1f)]
+	public float persistence = 0.5f;
+
+	public NoiseMethodType type;
+
 
 	private Texture2D texture;
 
@@ -44,7 +55,7 @@ public class TextureCreator : MonoBehaviour {
 		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
 		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
 
-		NoiseMethod method = Noise.valueMethods[dimensions - 1];
+		NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
 		float stepSize = 1f / resolution;
 		Random.seed = 42;
 
@@ -55,11 +66,16 @@ public class TextureCreator : MonoBehaviour {
 
 			for (int x = 0; x < resolution; x++) {
 				
-
 				Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
 
-				texture.SetPixel(x, y, Color.white * method(point, frequency));
+				//float sample = method(point, frequency);
+				float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+				if (type != NoiseMethodType.Value) {
+					sample = sample * 0.5f + 0.5f;
+				}
+				texture.SetPixel(x, y, Color.white * sample);
 
+				//texture.SetPixel(x, y, Color.white * method(point, frequency));
 				//texture.SetPixel(x, y, Color.white * Random.value);
 				//texture.SetPixel(x, y, new Color(point.x, point.y, point.z));
 				//texture.SetPixel(x, y, Color.green);
