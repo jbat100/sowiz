a041d54992459e9437631a79f21f028a3651adb1
Jonathan Thorpe <johnny.frenchy@gmail.com>
2016-04-03 19:54:14 +0200

    [unity] trying to work out sensible midi spawning patterns

--- unity/playground/Assets/Scripts/Manipulators/MidiTriggerManipulator.cs
+++ unity/playground/Assets/Scripts/Manipulators/MidiTriggerManipulator.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MidiTriggerManipulator : MonoBehaviour {
+public class MidiTriggerManipulator : SowizManipulator {
 
 	// Use this for initialization
 	void Start () {
-	
+
+		targetControlDelegates["midi"] = delegate(GameObject target, ArrayList values) {
+
+			MidiTrigger[] triggers = target.GetComponents<MidiTrigger>();
+
+			string midiMessageType = (string)values[0];
+
+			foreach(MidiTrigger trigger in triggers) {
+
+				switch (midiMessageType) {
+				case "note_on":
+
+				}
+
+				
+			}
+
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
---
+++ unity/playground/Assets/Scripts/Triggers/MidiNoteSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MidiNoteSpawnerElement : System.Object
+{
+	// TODO: have an option to spawn on spline or within volume instead of fixed StartTransform
+
+	public Transform StartTransform;
+	public GameObject Prefab;
+	public int Pitch; // -1 means default
+
+	public void Spawn(int channel, int pitch, int velocity) {
+		
+	}
+
+	public void Unspawn(int channel, int pitch, int velocity) {
+
+	}
+}
+
+public class MidiNoteSpawner : MidiTrigger {
+
+	public MidiNoteSpawnerElement[] Elements;
+
+	public int[] channels;
+
+	// apparently having a list (for inspector and serialization purposes), then building a dict for fast lookup 
+	// is sensible: http://answers.unity3d.com/questions/642431/dictionary-in-inspector.html
+
+	private Dictionary<int, MidiNoteSpawnerElement> ElementsByPitch;
+
+	// Use this for initialization
+	void Start () {
+
+		ElementsByPitch = new Dictionary<int, MidiNoteSpawnerElement>();
+
+		foreach (MidiNoteSpawnerElement element in Elements) {
+			
+		}
+
+		noteOnDelegate = delegate(int channel, int pitch, int velocity) {
+			MidiNoteSpawnerElement element = null;
+			if (ElementsByPitch.TryGetValue(pitch, out element))
+			{
+				element.Spawn(channel, pitch, velocity);
+			}
+		};
+	
+	}
+
+}
